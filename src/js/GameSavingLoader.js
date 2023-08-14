3db892cdb4dcc397f84c74b7bcfa5f5842fe392c
a.pushkin <a.v.pushkin@gmail.com>
2023-08-14 14:41:20 +0300

    add GameSave class

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,3 +1,4 @@
+import GameSaving from './GameSaving.js';
 import read from './reader.js';
 import json from './parser.js';
 
@@ -5,6 +6,7 @@ export default class GameSavingLoader {
   static load() {
     return read()
       .then((data) => json(data))
+      .then((data) => new GameSaving(data))
       .catch((error) => {
         throw new Error(`Ошибка при загрузке данных: ${error}`);
       });
